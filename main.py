6ff614c25095591848d276aa944c7d817c2261a3
Matt Lebrun <lebrun.matt@gmail.com>
2015-05-23 10:59:13 +0800

    Added an off-screen console to buffer the contents of the screen and created a generic object class to represent the game objects.

--- main.py
+++ main.py
@@ -10,12 +10,52 @@ playery = SCREEN_HEIGHT / 2
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE |
                                 libtcod.FONT_LAYOUT_TCOD)
 
-# Init the screen
+# Init the main screen
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'pyroguecod tutorial',
                           False)
 
+# Init an off-screen console to be used as a buffer
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
 # Set FPS. This does not have an effect for turn-based games
 libtcod.sys_set_fps(LIMIT_FPS)
+
+
+class Object(object):
+    """ Generic object class
+
+    Represents the player, monster, an item, the stairs, wall, etc. Its always
+    represented by a character on the screen.
+
+    """
+
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        """ Move by the given amount.
+
+        """
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        """ Set the color then draw the character that represents this object
+            at its position.
+
+        """
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char,
+                                 libtcod.BKGN_NONE)
+
+    def clear(self):
+        """ Erase the character that represents this object.
+
+        """
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGN_NONE)
 
 
 def handle_keys():
@@ -49,17 +89,20 @@ def main():
     while not libtcod.console_is_window_closed():
         global playerx, playery
 
-        # Set default text color to white
-        libtcod.console_set_default_foreground(0, libtcod.white)
+        # Set default off-screen text color to white
+        libtcod.console_set_default_foreground(con, libtcod.white)
 
-        # Place the main player on the screen
-        libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+        # Place the main player on the off-screen
+        libtcod.console_put_char(con, playerx, playery, '@', libtcod.BKGND_NONE)
+
+        # Blit the contents of the off-screen to the main screen
+        libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
         # Update the changes to the screen. Always keep this at the bottom.
         libtcod.console_flush()
 
-        # Clear the trailing character
-        libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+        # Clear the trailing character on the off-screen
+        libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
 
         # handle keys and exit the game if needed
         exit = handle_keys()
